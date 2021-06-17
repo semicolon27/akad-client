e7231543bd1ffac39e7e81381c3c28892c14eb03
ryznocaHub <fikrihalim27@gmail.com>
2021-06-17 16:16:41 +0700

    Update main Add connection to database

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,22 @@ import 'dart:html' as html;
 import 'dart:async';
 import 'dart:convert';
 // import 'package:http_parser/http_parser.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
-Future<Dokumen> createDokumen(int jenis, String noreg) async {
-  // print("createDokumen");
-  final msg = jsonEncode({"noreg": noreg, "jenis": jenis});
-  // print(msg);
+Future<Dokumen> createDokumen(int jenis, String noreg, var file) async {
+  String base64code = base64.encode(file);
+
+  final msg = jsonEncode({"noreg": noreg, "jenis": jenis, "file": base64code});
   final response = await http.post(
-    Uri.parse('http://127.0.0.1:8000/text'),
-    // Uri.parse('https://webhook.site/c502765b-dfce-405b-8bb3-b2c9c556ed43'),
+    // Uri.parse('http://127.0.0.1:8000/text'),
+    Uri.parse('https://webhook.site/c502765b-dfce-405b-8bb3-b2c9c556ed43'),
     headers: <String, String>{
       'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
     },
     body: msg,
   );
-
   if (response.statusCode == 201) {
     // print("status 201");
     return Dokumen.fromJson(jsonDecode(response.body));
@@ -73,14 +73,15 @@ class _MyHomePageState extends State<MyHomePage> {
   final TextEditingController noreg = TextEditingController();
   late int indexDokumen;
   var _jenisDokumen;
+  var fileBytes;
+  String fileName = "File belum dipilih";
+  GlobalKey _formKey = new GlobalKey();
 
   void pilihJenis(String value) {
     setState(() {
       _jenisDokumen = value;
     });
   }
-
-  GlobalKey _formKey = new GlobalKey();
 
   List jenisDokumen = [
     "KTP",
@@ -99,123 +100,126 @@ class _MyHomePageState extends State<MyHomePage> {
             Form(
               autovalidateMode: AutovalidateMode.always,
               key: _formKey,
-              child: Column(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Column(
-                        //mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Row(
-                              //mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Padding(
-                                  padding: EdgeInsets.only(top: 50, right: 145),
-                                  child: Text("Nomor Registrasi",
-                                      style: TextStyle(fontSize: 20)),
-                                ),
-                              ]),
-                          Row(
-                            //mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: <Widget>[
-                              Padding(
-                                padding: EdgeInsets.symmetric(
-                                    horizontal: 8, vertical: 16),
-                                child: Container(
-                                    height: 50,
-                                    width: 300,
-                                    child: TextField(
-                                      controller: noreg,
-                                      decoration: InputDecoration(
-                                        border: OutlineInputBorder(),
-                                        hintText: 'Masukan Inputan',
-                                      ),
-                                    )),
-                              ), //padding
-                            ],
-                          )
-                        ],
+                      Padding(
+                        padding: EdgeInsets.only(top: 50),
+                        child: Text("Nomor Registrasi",
+                            style: TextStyle(fontSize: 20)),
                       ),
-                      Column(children: [
-                        Row(children: [
-                          Padding(
-                            padding: EdgeInsets.only(top: 50, right: 160),
-                            child: Text("Jenis Dokumen",
-                                textAlign: TextAlign.left,
-                                style: TextStyle(fontSize: 20)),
-                          )
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 16),
+                        child: Container(
+                            height: 50,
+                            width: 300,
+                            child: TextField(
+                              controller: noreg,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                hintText: 'Masukan Inputan',
+                              ),
+                            )),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 50),
+                        child: Row(children: [
+                          ElevatedButton(
+                              onPressed: () async {
+                                var picked =
+                                    await FilePicker.platform.pickFiles(
+                                  type: FileType.custom,
+                                  allowedExtensions: [
+                                    'jpg',
+                                    'jpeg',
+                                    'png',
+                                    'pdf'
+                                  ],
+                                );
+                                if (picked != null) {
+                                  setState(() {
+                                    fileBytes = picked.files.first.bytes;
+                                    fileName = picked.files.first.name;
+                                  });
+
+                                  print(fileBytes);
+                                }
+                              },
+                              child: Text("Upload file")),
+                          Text(fileName)
                         ]),
-                        Row(
-                          children: [
-                            Padding(
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 50),
+                        child: Text("Jenis Dokumen",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(fontSize: 20)),
+                      ),
+                      Padding(
+                          padding: EdgeInsets.symmetric(vertical: 16),
+                          child: Container(
+                            height: 50,
+                            width: 300,
+                            decoration: BoxDecoration(
+                                border:
+                                    Border.all(color: Colors.grey, width: 1)),
+                            child: DropdownButton(
+                              hint: Padding(
                                 padding: EdgeInsets.symmetric(
                                     horizontal: 8, vertical: 16),
-                                child: Container(
-                                  height: 50,
-                                  width: 300,
-                                  decoration: BoxDecoration(
-                                      border: Border.all(
-                                          color: Colors.grey, width: 1)),
-                                  child: Padding(
-                                    padding: EdgeInsets.symmetric(
-                                        horizontal: 8, vertical: 16),
-                                    child: DropdownButton(
-                                      hint: Text("Pilih Jenis Dokumen"),
-                                      dropdownColor: Colors.white,
-                                      icon: Icon(Icons.arrow_drop_down),
-                                      isExpanded: true,
-                                      underline: SizedBox(),
-                                      value: _jenisDokumen,
-                                      onChanged: (newValue) {
-                                        setState(() {
-                                          _jenisDokumen = newValue!;
-                                          indexDokumen = jenisDokumen
-                                                  .indexOf(_jenisDokumen) +
-                                              1;
-                                        });
-                                        print(indexDokumen);
-                                      },
-                                      items: jenisDokumen.map((valueItem) {
-                                        return DropdownMenuItem(
-                                          value: valueItem,
-                                          child: Text(valueItem),
-                                        ); //DropdownMenuItem
-                                      }).toList(),
-                                    ),
-                                  ), //DropdownButton
-                                ) //Container
-                                ) //Padding
-                          ],
-                        ) //Row
-                      ]) //Column
-                    ],
-                  ), //Row Baris Utama 1
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Column(children: [Text("Dua Satu")]),
-                      Column(children: [
-                        Padding(
-                          padding: const EdgeInsets.all(50),
-                          child: Container(
-                            child: ElevatedButton.icon(
-                              icon: Icon(Icons.add),
-                              onPressed: () {
+                                child: Text("Pilih Jenis Dokumen"),
+                              ),
+                              dropdownColor: Colors.white,
+                              icon: Icon(Icons.arrow_drop_down),
+                              isExpanded: true,
+                              underline: SizedBox(),
+                              value: _jenisDokumen,
+                              onChanged: (newValue) {
                                 setState(() {
-                                  createDokumen(indexDokumen, noreg.text);
+                                  _jenisDokumen = newValue!;
+                                  indexDokumen =
+                                      jenisDokumen.indexOf(_jenisDokumen) + 1;
                                 });
+                                print(indexDokumen);
                               },
-                              label: Text('Tambah'),
+                              items: jenisDokumen.map((valueItem) {
+                                return DropdownMenuItem(
+                                  value: valueItem,
+                                  child: Text(valueItem),
+                                );
+                              }).toList(),
                             ),
-                          ),
-                        )
-                      ])
+                          )),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 50),
+                        child: ElevatedButton.icon(
+                          icon: Icon(Icons.add),
+                          onPressed: () {
+                            setState(() {
+                              createDokumen(
+                                  indexDokumen, noreg.text, fileBytes);
+                            });
+                          },
+                          label: Text('Tambah'),
+                        ),
+                      )
                     ],
                   ) // Row Baris Utama 2
                 ],
               ),
             ),
+            Container(
+                child: (fileBytes == null)
+                    ? Text("kosong")
+                    : Image.memory(fileBytes))
             // buatpreview
           ],
         ));
