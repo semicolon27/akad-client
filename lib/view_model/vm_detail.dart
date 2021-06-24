9a0871dcfe0e01e511bfd2b21f65a2f9770f5015
Putri Ayuni <putri.118140118@student.itera.ac.id>
2021-06-24 16:29:09 +0700

    Update Homepage

--- lib/view_model/vm_detail.dart
+++ lib/view_model/vm_detail.dart
@@ -1,13 +1,19 @@
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:stacked/stacked.dart';
+import 'package:pdf/widgets.dart' as pw;
 
 class DetailVM extends BaseViewModel {
   GlobalKey formkey = GlobalKey();
   final TextEditingController noreg = TextEditingController();
+  final doc = pw.Document();
   var fileBytes;
   var indexDokumen;
   var jenis;
+  // ignore: unused_field
+
+  
   // int? indexDokumen;
   // String? jenis;
   String fileName = "File belum dipilih";
@@ -20,12 +26,14 @@ class DetailVM extends BaseViewModel {
   void pilihFile() async {
     var picked = await FilePicker.platform.pickFiles(
       type: FileType.custom,
-      allowedExtensions: ['jpg', 'jpeg', 'png'],
+      allowedExtensions: ['jpg', 'jpeg', 'png', 'pdf'],
     );
     if (picked != null) {
       fileBytes = picked.files.first.bytes;
       fileName = picked.files.first.name;
       notifyListeners();
+    } else {
+      
     }
   }
 
@@ -34,4 +42,6 @@ class DetailVM extends BaseViewModel {
     indexDokumen = jenisDokumen.indexOf(jenis) + 1;
     notifyListeners();
   }
+
+
 }
