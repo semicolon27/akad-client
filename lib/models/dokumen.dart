1f66949322377b96fa2295b72ed9b0c09fa34b52
ryznocaHub <fikrihalim27@gmail.com>
2021-06-25 09:36:29 +0700

    Update Model Dokumen Add attribut nama

--- lib/models/dokumen.dart
+++ lib/models/dokumen.dart
@@ -1,22 +1,24 @@
 class Dokumen {
   final int id;
   final int jenis;
+  final String nama;
   final String noreg;
   final file;
 
   Dokumen(
       {required this.id,
       required this.jenis,
+      required this.nama,
       required this.noreg,
       required this.file});
 
   factory Dokumen.fromJson(Map<String, dynamic> json) {
-    print("factory fromJson");
     return Dokumen(
         //bisa ganti key sesuai kebutuhan dengan
         //dengan catatan yang diambil pada object adalah nama colom di DB
         id: json['id'],
         jenis: json['jenis'],
+        nama: json['nama'],
         noreg: json['noreg'],
         file: json['file']);
   }
