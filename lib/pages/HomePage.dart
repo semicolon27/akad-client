6b0873e2f2fcf5110dcaef06d219850ac81d8927
Putri Ayuni <putri.118140118@student.itera.ac.id>
2021-06-29 09:26:12 +0700

    Merge branch 'master' of https://gitlab.mglobalperdana.com:12001/mitrasoft-pkl21/akad-client into putri

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -21,49 +21,60 @@ class _HomeState extends State<Home> {
     // cuma perlu di bungkus saja
     return TemplateWidget(
       child: Scaffold(
-          body: ViewModelBuilder<HomeVM>.reactive(
+        body: ViewModelBuilder<HomeVM>.reactive(
         viewModelBuilder: () => HomeVM(),
-        builder: (context, model, child) => FutureBuilder<List<Doklist>>(
-          // child: FutureBuilder<List<Doklist>>(
-          future: readDoklist(),
-          builder: (context, snapshot) {
-            if (snapshot.data == null) {
-              return Container(child: Text("Loading"));
-            } else
-              return DataTable(
-                //data secara horizontal
-                columns: <DataColumn>[
-                  DataColumn(label: Text("No. Registrasi")),
-                  DataColumn(label: Text("Nama")),
-                  DataColumn(label: Text("Jenis Dokumen")),
-                  DataColumn(label: Text("Aksi")),
-                ],
-
-                //data secara vertikal
-                rows: snapshot.data!
-                    .map(
-                      (data) => DataRow(
-                        cells: [
-                          DataCell(Text(data.noreg)),
-                          DataCell(Text(data.nama)),
-                          DataCell(Text(data.jenis)),
-                          DataCell(ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                onSurface: Colors.blue[600]),
-                            onPressed: null,
-                            child: Text('Detail'),
-                          )),
+        builder: (context, model, child) => Container(
+          margin: new EdgeInsets.all(30),
+          child: Column(
+            children: [
+              SizedBox(
+                width: double.infinity,
+                height: double.infinity,
+                child: FutureBuilder<List<Doklist>>(
+                  // child: FutureBuilder<List<Doklist>>(
+                  future: readDoklist(),
+                  builder: (context, snapshot) {
+                    if (snapshot.data == null) {
+                      return Container(child: Text("Loading"));
+                    } else
+                      return DataTable(
+                        //data secara horizontal
+                        columns: <DataColumn>[
+                          DataColumn(label: Text("No. Registrasi")),
+                          DataColumn(label: Text("Nama")),
+                          DataColumn(label: Text("Jenis Dokumen")),
+                          DataColumn(label: Text("Aksi")),
                         ],
-                      ),
-                    )
-                    .toList(),
-              );
-            // return ListTile(
-            //   title: Text(snapshot.data![i].nama),
-            //   subtitle: Text(snapshot.data![i].noreg),
-            // );
-            // });
-          },
+
+                        //data secara vertikal
+                        rows: snapshot.data!
+                            .map(
+                              (data) => DataRow(
+                                cells: [
+                                  DataCell(Text(data.noreg)),
+                                  DataCell(Text(data.nama)),
+                                  DataCell(Text(data.jenis)),
+                                  DataCell(ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                        onSurface: Colors.blue[600]),
+                                    onPressed: null,
+                                    child: Text('Detail'),
+                                  )),
+                                ],
+                              ),
+                            )
+                            .toList(),
+                      );
+                    // return ListTile(
+                    //   title: Text(snapshot.data![i].nama),
+                    //   subtitle: Text(snapshot.data![i].noreg),
+                    // );
+                    // });
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       )),
     );
