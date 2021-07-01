f4230606332e12951ba06b9314c344f237f9eac5
Putri Ayuni <putri.118140118@student.itera.ac.id>
2021-07-01 16:15:18 +0700

    Update Login in Validation

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -10,6 +10,9 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  //Global Key belum dipindah ke vm
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,104 +48,125 @@ class _LoginPageState extends State<LoginPage> {
                           borderRadius: BorderRadius.circular(20),
                           shape: BoxShape.rectangle,
                         ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Row(
-                             mainAxisAlignment: MainAxisAlignment.center,
-                             children: [
-                              Text(
-                                  " LOG IN ",
-                                  style: TextStyle(
-                                  color: Colors.blueAccent,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 40,
-                              )),
-                             ],
-                            ),
+                        child: Form(
+                          key: _formKey,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Row(
+                               mainAxisAlignment: MainAxisAlignment.center,
+                               children: <Widget> [
+                                Text(
+                                    " LOG IN ",
+                                    style: TextStyle(
+                                    color: Colors.blueAccent,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 40,
+                                )),
+                               ],
+                              ),
+                              Padding(
+                              padding: EdgeInsets.all(10)),
+                              Row(
+                               mainAxisAlignment: MainAxisAlignment.center,
+                               children: [
+                                Text(
+                                  "Silahkan masuk ke Dasboard Admin",
+                                    style: TextStyle(
+                                    color: Colors.blue[100],
+                                    fontSize: 15,
+                                )),
+                               ],
+                              ),
+                              Padding(
+                              padding: EdgeInsets.all(30)),
+                              TextFormField(
+                                autocorrect: false,
+                                autofocus: false,
+                                style: TextStyle(
+                                  fontSize: 18,
+                                ),
+                                decoration: InputDecoration(
+                                  hintText: 'Enter your e-mail',
+                                  labelText: 'Username',
+                                  border: InputBorder.none,
+                                  filled: true,
+                                  fillColor: Colors.grey[200],
+                                  contentPadding: EdgeInsets.all(10.0),
+                                  prefixIcon: Icon(Icons.email),
+                                ),
+                                validator: (value) {
+                                  if(value == null || value.isEmpty){
+                                    return 'Please enter field';
+                                  }
+                                  return null; 
+                                },
+                              ),
+                              Padding(
+                                padding: EdgeInsets.symmetric(
+                                  vertical:25,
+                                ),
+                              child: TextFormField(
+                                autocorrect: false,
+                                autofocus: false,
+                                obscureText: false,
+                                style: TextStyle(
+                                  fontSize: 18,
+                                ),
+                                decoration: InputDecoration(
+                                  hintText: 'Password',
+                                  labelText: 'Password',
+                                  border: InputBorder.none,
+                                  filled: true,
+                                  fillColor: Colors.grey[200],
+                                  contentPadding: EdgeInsets.all(10.0),
+                                  prefixIcon: Icon(Icons.lock),
+                                ),
+                                validator: (value) {
+                                  if(value == null || value.isEmpty){
+                                    return 'Please enter field';
+                                  }
+                                  return null; 
+                                },
+                              ),
+                             ),
+                             Row(
+                               mainAxisAlignment: MainAxisAlignment.end,
+                               children: [
+                                 Text(
+                                   'Forget Password?',
+                                   style: TextStyle(
+                                     color: Colors.blueAccent,
+                                     fontSize: 14,
+                                   ),
+                                 ),
+                               ],
+                             ),
                             Padding(
-                            padding: EdgeInsets.all(10)),
-                            Row(
-                             mainAxisAlignment: MainAxisAlignment.center,
-                             children: [
-                              Text(
-                                "Silahkan masuk ke Dasboard Admin",
-                                  style: TextStyle(
-                                  color: Colors.blue[100],
-                                  fontSize: 15,
-                              )),
-                             ],
+                              padding: EdgeInsets.all(10),
                             ),
-                            Padding(
-                            padding: EdgeInsets.all(30)),
-                            TextField(
-                              autocorrect: false,
-                              autofocus: false,
-                              style: TextStyle(
-                                fontSize: 18,
-                              ),
-                              decoration: InputDecoration(
-                                hintText: 'Enter your e-mail',
-                                labelText: 'Username',
-                                border: InputBorder.none,
-                                filled: true,
-                                fillColor: Colors.grey[200],
-                                contentPadding: EdgeInsets.all(10.0),
-                              ),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.symmetric(
-                                vertical:25,
-                              ),
-                              child: TextField(
-                              autocorrect: false,
-                              autofocus: false,
-                              obscureText: false,
-                              style: TextStyle(
-                                fontSize: 18,
-                              ),
-                              decoration: InputDecoration(
-                                hintText: 'Password',
-                                labelText: 'Password',
-                                border: InputBorder.none,
-                                filled: true,
-                                fillColor: Colors.grey[200],
-                                contentPadding: EdgeInsets.all(10.0),
-                              ),
-                            ),
-                           ),
-                           Row(
-                             mainAxisAlignment: MainAxisAlignment.end,
-                             children: [
-                               Text(
-                                 'Forget Password?',
+                            MaterialButton(
+                               onPressed: (){
+                                if (_formKey.currentState!.validate()){
+                                   ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:Text('Processing Data')));
+                                 }
+                               },
+                               minWidth: 250,
+                               splashColor: Colors.green,
+                               color: Colors.blueAccent,
+                               padding: EdgeInsets.symmetric(
+                                 vertical: 12,
+                               ),
+                               child: Text(
+                                 'Login',
                                  style: TextStyle(
-                                   color: Colors.blueAccent,
-                                   fontSize: 14,
+                                   color: Colors.white,
+                                   fontSize: 15,
                                  ),
                                ),
-                             ],
-                           ),
-                          Padding(
-                            padding: EdgeInsets.all(10),
+                             ),
+                            ],
                           ),
-                           MaterialButton(
-                             onPressed: (){},
-                             minWidth: 250,
-                             splashColor: Colors.green,
-                             color: Colors.blueAccent,
-                             padding: EdgeInsets.symmetric(
-                               vertical: 12,
-                             ),
-                             child: Text(
-                               'Login',
-                               style: TextStyle(
-                                 color: Colors.white,
-                                 fontSize: 15,
-                               ),
-                             ),
-                           ),
-                          ],
                         ),
                     )),
                   ],
