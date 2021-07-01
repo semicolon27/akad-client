ab81ce3c654862da295a59d52cfceb1322afca4d
ryznocaHub <fikrihalim27@gmail.com>
2021-07-01 17:20:14 +0700

    Update SideBar Route Add new list Tambah Dokumen

--- lib/core_widget/navigation/sidebar_route_list.dart
+++ lib/core_widget/navigation/sidebar_route_list.dart
@@ -2,10 +2,19 @@ import 'package:flutter_icons/flutter_icons.dart';
 
 List<Map<String, dynamic>> sidebarItem = [
   {
-    "title": "Tabel Dokumen",
+    "title": "Home",
     "route": "/home",
-    "icon": MaterialCommunityIcons.file_outline,
-    "iconActive": MaterialCommunityIcons.file,
+    "icon": MaterialCommunityIcons.home,
+    "iconActive": MaterialCommunityIcons.home_outline,
+    "svg": null,
+    "svgActive": null,
+    "children": [],
+  },
+  {
+    "title": "Tambah Dokumen",
+    "route": "/tambah",
+    "icon": MaterialCommunityIcons.file_document_box_plus,
+    "iconActive": MaterialCommunityIcons.file_document_box_plus_outline,
     "svg": null,
     "svgActive": null,
     "children": [],
