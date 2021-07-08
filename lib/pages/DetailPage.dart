81e845488834430b9a10bd110b929ba9cc7ed686
Putri Ayuni <putri.118140118@student.itera.ac.id>
2021-07-08 12:58:41 +0700

    Update Halaman  Detail

--- lib/pages/DetailPage.dart
+++ lib/pages/DetailPage.dart
@@ -28,78 +28,103 @@ class _DetailDokumenState extends State<DetailDokumen> {
           builder: (context, model, child) => !model.busy(model.data)
               ? SingleChildScrollView(
                   child: Container(
-                    padding: EdgeInsets.all(16.0),
+                    margin: EdgeInsets.fromLTRB(100, 40, 160, 90),
+                    padding: EdgeInsets.fromLTRB(40, 30, 20, 30),
                     decoration: BoxDecoration(
                       border: Border.all(
                         color: Colors.white,
-                        width: 15,
+                        width: 5,
                       )
                     ),
                     child: Column(
-                      children: [
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
                         Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Padding(
-                                  padding: EdgeInsets.only(top: 50),
-                                  child: Text("Nomor Registrasi",
-                                      style: TextStyle(fontSize: 20)),
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.symmetric(vertical: 16),
-                                  child: Text(model.data.first.noreg,
-                                      style: TextStyle(fontSize: 18)),
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.only(top: 50),
-                                  child: Text("Keterangan Dokumen",
-                                      style: TextStyle(fontSize: 20)),
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.symmetric(vertical: 16),
-                                  child: (model.data.first.keterangan == '')
-                                      ? Text("-")
-                                      : Text(model.data.first.keterangan ?? "",
-                                          style: TextStyle(fontSize: 18)),
-                                ),
-                              ],
+                            Padding(
+                              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                              child: Text("Nomor Registrasi",
+                                style: TextStyle(fontSize: 20)),
+                              ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+                              child: Text(model.data.first.noreg,
+                              style: TextStyle(fontSize: 18)),
                             ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Padding(
-                                  padding: EdgeInsets.only(top: 50),
-                                  child: Text("Jenis Dokumen",
-                                      textAlign: TextAlign.left,
-                                      style: TextStyle(fontSize: 20)),
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.symmetric(vertical: 16),
-                                  child: Text(model.data.first.jenis,
-                                      style: TextStyle(fontSize: 18)),
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.only(top: 50),
-                                  child: Text("File Dokumen",
-                                      textAlign: TextAlign.left,
-                                      style: TextStyle(fontSize: 20)),
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.symmetric(vertical: 16),
-                                  child: Text(model.data.first.nama,
-                                      style: TextStyle(fontSize: 18)),
-                                ),
-                              ],
-                            ) // Row Baris Utama 2
                           ],
                         ),
                         Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Padding(padding: EdgeInsets.all(10)),
+                            Padding(
+                              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                              child: Text("Keterangan Dokumen",
+                              style: TextStyle(fontSize: 20)),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+                              child: (model.data.first.keterangan == '')
+                                ? Text("-")
+                                  : Text(model.data.first.keterangan ?? "",
+                                    style: TextStyle(fontSize: 18)),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                                child: Text("Jenis Dokumen",
+                                textAlign: TextAlign.left,
+                                style: TextStyle(fontSize: 20)),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 70),
+                                child: Text(model.data.first.jenis,
+                                style: TextStyle(fontSize: 18)),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                              child: Text("File Dokumen",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(fontSize: 20)),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 82.5),
+                              child: Text(model.data.first.nama,
+                              style: TextStyle(fontSize: 18)),
+                            ),
+                          ],
+                        ),      
+                        Container(
+                          margin: EdgeInsets.only(left: 82.5),
+                            child: (model.data.first.file == null)
+                                ? Text(" ")
+                                : (model.data.first.extension == 'pdf')
+                                    ? Container(
+                                        width: 650,
+                                        height: 400,
+                                        margin:
+                                            const EdgeInsetsDirectional.all(25),
+                                        child: PdfPreview(
+                                          build: (newFile) => generatePDF(
+                                              newFile, model.data.first.file),
+                                        ))
+                                    : Container(
+                                        width: 650,
+                                        height: 400,
+                                        margin:
+                                            const EdgeInsetsDirectional.all(25),
+                                        child:
+                                            Image.memory(model.data.first.file))),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.fromLTRB(380, 5, 100, 5)),
                             ElevatedButton(
                               onPressed: () => {
                                 Get.toNamed(
@@ -120,26 +145,6 @@ class _DetailDokumenState extends State<DetailDokumen> {
                             ),
                           ],
                         ),
-                        Container(
-                            child: (model.data.first.file == null)
-                                ? Text(" ")
-                                : (model.data.first.extension == 'pdf')
-                                    ? Container(
-                                        width: 650,
-                                        height: 400,
-                                        margin:
-                                            const EdgeInsetsDirectional.all(25),
-                                        child: PdfPreview(
-                                          build: (newFile) => generatePDF(
-                                              newFile, model.data.first.file),
-                                        ))
-                                    : Container(
-                                        width: 650,
-                                        height: 350,
-                                        margin:
-                                            const EdgeInsetsDirectional.all(25),
-                                        child:
-                                            Image.memory(model.data.first.file)))
                       ],
                     ),
                   ),
